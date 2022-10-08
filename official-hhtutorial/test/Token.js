e0b97d45611c8b5c6a5a311fd4c580b74a59accc
Lauren Morgan <rg326@live.com>
2022-10-07 20:41:18 -0400

    added chai test file for Token.sol in official-hhtutorial

---
+++ official-hhtutorial/test/Token.js
@@ -0,0 +1,15 @@
+const {expect} = require('chai');
+
+describe('Token contract',()=>{
+    it('Deployment should assign the total supply of tokens to the owner', async () => {
+    //MUST use async/await for all transactions done on the blockchain
+        const [owner] = await ethers.getSigners();
+
+        const Token = await ethers.getContractFactory('Token');
+
+        const hardhatToken = await Token.deploy();
+
+        const ownerBalance = await hardhatToken.balanceOf(owner.address);
+        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    })
+})
