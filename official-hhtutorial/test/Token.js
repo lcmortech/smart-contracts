30591aee9c52dd7166fe08a7b48e66322d212860
Lauren Morgan <rg326@live.com>
2022-10-09 23:00:14 -0400

    added notes to token.js in official-hhtutorial

--- official-hhtutorial/test/Token.js
+++ official-hhtutorial/test/Token.js
@@ -8,6 +8,7 @@ describe('Token contract',()=>{
         //A Signer in ethers.js is an object that represents an Ethereum account. It's used to send transactions to contracts and other accounts. Here we're getting a list of the accounts in the node we're connected to, which in this case is Hardhat Network, and we're only keeping the first one.
         const [owner] = await ethers.getSigners();
 
+        //A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts, so Token here is a factory for instances of our token contract.
         const Token = await ethers.getContractFactory('Token');
 
         const hardhatToken = await Token.deploy();
