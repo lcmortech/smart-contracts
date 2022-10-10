120dd3e8e992506abc0202482e42b4399d6c2ec1
Lauren Morgan <rg326@live.com>
2022-10-09 22:50:12 -0400

    added notes to token.js in official-hhtutorial

--- official-hhtutorial/test/Token.js
+++ official-hhtutorial/test/Token.js
@@ -4,6 +4,8 @@ const ethers = require('ethers');
 describe('Token contract',()=>{
     it('Deployment should assign the total supply of tokens to the owner', async () => {
     //MUST use async/await for all transactions done on the blockchain
+
+        //A Signer in ethers.js is an object that represents an Ethereum account. It's used to send transactions to contracts and other accounts. Here we're getting a list of the accounts in the node we're connected to, which in this case is Hardhat Network, and we're only keeping the first one.
         const [owner] = await ethers.getSigners();
 
         const Token = await ethers.getContractFactory('Token');
