382249dd61641606885e1e93d52d8c971ef63c83
Lauren Morgan <rg326@live.com>
2022-10-13 21:59:44 -0400

    added notes to token-test.js in official-hhtutorial

--- official-hhtutorial/test/Token-test.js
+++ official-hhtutorial/test/Token-test.js
@@ -1,3 +1,5 @@
+// If you need to test your code by sending a transaction from an account (or Signer in ethers.js terminology) other than the default one, you can use the connect() method on your ethers.js Contract object to connect it to a different account, like this:
+
 const { expect } = require("chai");
 
 describe("Token contract", function () {
