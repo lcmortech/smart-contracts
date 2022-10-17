a6a41393dd8b8fae13863faf07120b72c488e6a7
Lauren Morgan <rg326@live.com>
2022-10-16 20:39:08 -0400

    added token.sol smart contract to etb-hhtutorial

--- etb-hhtutorial/Token.sol
+++ etb-hhtutorial/Token.sol
@@ -1,1 +1,19 @@
 pragma solidity ^0.8.0;
+
+contract Token {
+    string public name = 'My Hardhat Token';
+    string public symbol = 'MHT';
+    uint public totalSupply = 1000000;
+    address public owner;
+    mapping (address => uint);
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint amount) external {
+        require(balances[msg.sender] >= amount, 'Not enough tokens');
+    }
+
+}
